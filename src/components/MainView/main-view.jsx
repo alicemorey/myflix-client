4f752265b9599bda007a19ebfce4bdc3a40cce5b
alicemorey <alicemmorey@gmail.com>
2024-05-21 20:06:44 +0200

    adding api

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,35 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-        id: 1,
-        title: "The Ring",
-        description:"Teenage girls Katie and Becca discuss an urban legend about a cursed videotape that causes whoever watches it to die ins even days. That night, Katie, who watched it a week ago, is killed by an unseen force.",
-        year:"2002",
-        genre:"Horror",
-        director: "Gore Verbinski",
-        image: "https://en.wikipedia.org/wiki/The_Ring_(2002_film)#/media/File:Theringpostere.jpg", 
-    },
-    {   id: 2, 
-        title: "Great Expectations",
-        description:"A contemporary film adaptation of Charles Dickens 1861 novel of the same name, it is known for having moved the setting of the original novel from 1812-1827 London to 1990s New York.",
-        year:"1998",
-        genre:"Romance", 
-        image:"https://en.wikipedia.org/wiki/Great_Expectations_(1998_film)#/media/File:Great_expectations_poster.jpg",
-        director: "Alfonso Cuarón"
-     },
-    {   id: 3, 
-        title: "Alien:Covenant" , 
-        description:"A joint American and British production, it is a sequel to Prometheus (2012), the second entry in the Alien prequel series, and the sixth installment in the series but counting crossovers eighth film in the overall Alien franchise (three of which have been directed by Scott)", 
-        year:"2017",
-        genre:"Horror",
-        image:"https://en.wikipedia.org/wiki/Alien:_Covenant#/media/File:Alien_Covenant_Teaser_Poster.jpg",
-        director: "Ridley Scott"
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  
+  useEffect=(()=>{
+    fetch ("https://myflix-movies2024-b07bf2b16bbc.herokuapp.com/")
+    .then((response) => response.json())
+      .then((data) => {
+        console.log("movies from api:", data);
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            genre: doc.genre,
+            director: doc.director_name?.[0],
+            image: "",
+          };
+        });
+        setBooks(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
@@ -43,7 +35,6 @@ export const MainView = () => {
       />
     );
   }
-  
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
