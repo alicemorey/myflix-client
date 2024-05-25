ee3ceb75553b506d5ac2ad1baf6096e89a432026
alicemorey <alicemmorey@gmail.com>
2024-05-25 14:52:47 +0200

    adding react

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/MainView/main-view';
 
